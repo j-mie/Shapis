160f92f93109598f400159ace57af9a61e019635
Jamie Hankins <jamiehankin@gmail.com>
2013-12-23 21:21:33 +0000

    Commit code

---
+++ Shapis/GetWhois.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapis
+{
+    /// <summary>
+    /// Get a WHOIS for a given domain.
+    /// </summary>
+    class GetWhois
+    {
+        private WhoisRecord getServer(WhoisRecord record)
+        {
+            var serverGetter = new GetWhoisServer();
+            record.Server = serverGetter.GetServer(record.Domain);
+            return record;
+        }
+
+        /// <summary>
+        /// Get information on a particular domain
+        /// </summary>
+        /// <param name="record"></param>
+        /// <returns></returns>
+        public WhoisRecord FillRecord(WhoisRecord record)
+        {
+            record.Server = getServer(record).Server;
+            var tcpClient = new TCPClient();
+            record.Text = tcpClient.Read(record.Server, 43, record.Domain);
+
+            return record;
+        }
+    }
+}
